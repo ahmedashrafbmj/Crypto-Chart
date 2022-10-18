1e8a8e2f9d3233cc10c5242cd0e154e99cbe4e6e
ahmedashrafbmj <ahmedzain7557@gmail.com>
2022-10-18 14:05:00 +0500

    first commit

---
+++ build/precache-manifest.3c26de197eea34a66d2458c85d0850ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec197147bc2cd9d997384cc45e39e8f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d377419b09d7a3f8e2a",
+    "url": "/static/css/main.8e0254cd.chunk.css"
+  },
+  {
+    "revision": "72f9774b3ed54f8ccf21",
+    "url": "/static/js/2.6db2a02b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.6db2a02b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d377419b09d7a3f8e2a",
+    "url": "/static/js/main.13d1fed7.chunk.js"
+  },
+  {
+    "revision": "866ed326f9b633faeba8",
+    "url": "/static/js/runtime-main.0d63bdcd.js"
+  }
+]);
